f870a6b061e9b144a416ff82fa0d2481d8a4c9ce
isah ibrahim <devetecho@gmail.com>
2022-09-19 09:39:13 +0100

    First commit

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+  <div class="w3-container w3-teal">
+     <h2>Login Form</h2>
+  </div>
+
+  <form class="w3-container" method="post" action="">
+     <label class="w3-text-teal"><b>Email</b></label>
+     <input class="w3-input w3-border w3-light-grey" style="width: 40%;" type="text" name="email" >
+
+     <label class="w3-text-teal"><b>Password</b></label>
+     <input class="w3-input w3-border w3-light-grey" style="width: 40%;" type="password" name="password">
+
+     <button class="w3-btn w3-blue-grey w3-pt-6">Login</button>
+  </form>
+
+
+    
+</body>
+</html>

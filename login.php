f699f6321f231c95a0bec14917a8c356ac6fe716
isah ibrahim <devetecho@gmail.com>
2022-09-28 15:10:09 +0100

    login page

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php 
+   define('__CONFIG__',true);
+   require_once "inc/config.php"
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,22 +13,30 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 </head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 <body>
   <div class="w3-container w3-teal">
      <h2>Login Form</h2>
   </div>
 
-  <form class="w3-container" method="post" action="">
+  <form class="w3-container w3-login" method="post" action="" >
      <label class="w3-text-teal"><b>Email</b></label>
-     <input class="w3-input w3-border w3-light-grey" style="width: 40%;" type="text" name="email" >
+     <input class="w3-input w3-border w3-light-grey" style="width: 40%;" type="email" name="email" required placeholder="login@email.com">
 
      <label class="w3-text-teal"><b>Password</b></label>
-     <input class="w3-input w3-border w3-light-grey" style="width: 40%;" type="password" name="password">
+     <input class="w3-input w3-border w3-light-grey" style="width: 40%;" type="password" name="password" required placeholder="Password"> 
 
-     <button class="w3-btn w3-blue-grey w3-pt-6">Login</button>
+     <button class="w3-btn w3-blue-grey w3-pt-6" type="submit">Login</button>
+
+     <div class="w3-panel w3-red  w3-error" style="width:29%;  display:none;"></div>
+
+
   </form>
 
-
     
+  
+  <?php require "inc/footer.php"; ?>    
+  
 </body>
-</html>
+</html> 

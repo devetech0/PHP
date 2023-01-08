1e5438f8a6c3fd26cd18e76b35ba9ad67260ebb7
isah ibrahim <devetecho@gmail.com>
2023-01-08 15:46:27 +0100

    files

--- inc/classes/db.php
+++ inc/classes/db.php
@@ -12,7 +12,7 @@ class DB {
 
 		try {
 
-			 self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=university', 'root', 'blacksea' );
+			 self::$con = new PDO( 'mysql:charset=utf8mb4;host=localhost;port=3306;dbname=university', 'root', '*******' );
 			 self::$con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 			 self::$con->setAttribute( PDO::ATTR_PERSISTENT, false );
 
